19ead397a5d47008aafaceb5bae69421e8eeecd1
sparklep <sparklep519@gmail.com>
2024-04-18 15:45:27 +0900

    Basic Project Setup

--- src/main/java/com/sparklep/preorder/PreOrderShopApplication.java
+++ src/main/java/com/sparklep/preorder/PreOrderShopApplication.java
@@ -2,8 +2,9 @@ package com.sparklep.preorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PreOrderShopApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/sparklep/preorder/TestContorller.java
@@ -0,0 +1,12 @@
+package com.sparklep.preorder;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/hello")
+public class TestContorller {
+
+//    public String getHello() {
+//        return "Test Pass";
+//    }
+}
