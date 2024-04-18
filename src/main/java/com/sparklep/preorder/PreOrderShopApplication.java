d55a888fc52bdf322df271fcdf60a64ec48d04dc
sparklep <sparklep519@gmail.com>
2024-04-18 15:27:17 +0900

    Basic Project Setup

---
+++ src/main/java/com/sparklep/preorder/PreOrderShopApplication.java
@@ -0,0 +1,13 @@
+package com.sparklep.preorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOrderShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreOrderShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparklep/preorder/PreOrderShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparklep.preorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOrderShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
