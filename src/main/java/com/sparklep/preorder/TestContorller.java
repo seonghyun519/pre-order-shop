560c0da20029cec755f2211bece08c229b6a125a
sparklep <sparklep519@gmail.com>
2024-04-18 16:00:45 +0900

    feat: 추후 사용할 로컬타임 지역 시간대 (Asia/Seoul) 설정 스프링 시큐리티 기본 로그인 페이지 비활성화

--- src/main/java/com/sparklep/preorder/TestContorller.java
+++ src/main/java/com/sparklep/preorder/TestContorller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class TestContorller {
 
-//    public String getHello() {
-//        return "Test Pass";
-//    }
+    public String getHello() {
+        return "Test Pass";
+    }
 }
