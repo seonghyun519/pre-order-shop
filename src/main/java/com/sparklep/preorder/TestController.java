e2edb6bc4982c89cd2c82b9ac199f37b131fe4b6
sparklep <sparklep519@gmail.com>
2024-04-19 19:39:43 +0900

    readme

--- src/main/java/com/sparklep/preorder/TestController.java
+++ src/main/java/com/sparklep/preorder/TestController.java
@@ -1,10 +1,10 @@
 package com.sparklep.preorder;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String getHello() {
         return "Test Pass";
     }
